e7d136b31b4484e749d70043f0002dc07b881916
hnihTyoB <nctmdt@gmail.com>
2026-07-25 17:41:50 +0700

    fix(be-analytics): include unassigned tasks in task status overview statistics

--- src/modules/tasks/task.analytics.service.ts
+++ src/modules/tasks/task.analytics.service.ts
@@ -23,7 +23,10 @@ export class TaskAnalyticsService {
 
     const assignmentWhere: any = {
       task: { deletedAt: null },
-      intern: { deletedAt: null },
+      OR: [
+        { internId: null },
+        { intern: { deletedAt: null } }
+      ]
     };
     if (taskGroupId) {
       assignmentWhere.task = { deletedAt: null, taskGroupId: taskGroupId };
